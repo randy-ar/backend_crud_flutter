896a867d88d035d2c71a5949687721413887420d
randy-ar <randyabdulrahman87@gmail.com>
2025-08-25 09:19:37 +0700

    fix login error redirect

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json('Hello World!');
 });
+
+Route::get('/login', function () {
+    return response()->json('Please Login!');
+})->name('login');
