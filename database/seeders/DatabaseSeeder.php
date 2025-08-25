01d46c486756dc5b8cddb88bd79a31b0a02df7ba
Frederick Cornelius Nathaniel <123253241+r4tatouille@users.noreply.github.com>
2025-08-25 09:42:02 +0700

    Perubahan di DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +17,15 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'rheiya',
+            'email' => 'kardon@gmail.com',
+            'password' => Hash::make("25092002")
+        ]);
+
+        User::factory()->create([
+            'name' => 'randy',
+            'email' => 'rendy@gmail.com',
+            'password' => Hash::make("112341234")
         ]);
     }
 }
